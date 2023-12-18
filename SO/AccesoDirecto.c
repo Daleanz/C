104883fc6c982b6ac2cf8485d6a1343f643ec8f8
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-12-18 03:02:19 -0300

    Se acutalizo codigo: AccesoDirecto,c 03:02 am
    Codigo actualizado.

--- SO/AccesoDirecto.c
+++ SO/AccesoDirecto.c
@@ -4,20 +4,21 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+# include <stdbool.h>
 
 struct Persona{
-    char nombre[50];
-    char apellido[50];
-    char empresa[50];
-    char direccion[50];
-    char ciudad[50];
-    char pais[50];
-    char estado[50];
-    char codigoPostal[50];
-    char telefono1[50];
-    char telefono2[50];
-    char email[50];
-    char web[50];
+    char nombre[100];
+    char apellido[100];
+    char empresa[100];
+    char direccion[100];
+    char ciudad[100];
+    char pais[100];
+    char provincia[100];
+    char codigoPostal[100];
+    char telefono1[100];
+    char telefono2[100];
+    char email[100];
+    char web[100];
 };
 
 void cuentaRegresiva(int valor){
@@ -42,7 +43,7 @@ FILE *abrirArchivo(const char *nombreArchivo, const char *modo){
     FILE *archivo = fopen(nombreArchivo, modo);
 
     if(archivo == NULL){
-        perror("No se pudo abrir el archiv.");
+        perror("No se pudo abrir el archivo.");
         exit(EXIT_FAILURE);
     }
     return archivo;
@@ -62,7 +63,6 @@ void buscarPorEmail(FILE *archivo, const char *email){
     fseek(archivo, 0, SEEK_SET);
 
     printf("\n");
-    printf("Buscando por el siguiente email: %s\n\n", persona.email);
     cuentaRegresiva(1);
     while(fread(&persona, sizeof(struct Persona), 1, archivo) == 1){
         if(strcmp(persona.email, email) == 0){
@@ -85,7 +85,6 @@ void modificarRegistroPersona(FILE *archivo, const char *email, int campo, const
 
     fseek(archivo, 0, SEEK_SET);  // Ir al principio del archivo
 
-    printf("Modificando registro...\n");
     cuentaRegresiva(2);
     while (fread(&persona, sizeof(struct Persona), 1, archivo) == 1) {
         if (strcmp(persona.email, email) == 0) {
@@ -102,7 +101,33 @@ void modificarRegistroPersona(FILE *archivo, const char *email, int campo, const
                 case 3: // company
                     strncpy(persona.empresa, nuevoValor, sizeof(persona.empresa) - 1);
                     break;
-                // Agrega más casos según sea necesario para otros campos
+                case 4:
+                    strncpy(persona.direccion, nuevoValor, sizeof(persona.direccion) - 1);
+                    break;
+                case 5:
+                    strncpy(persona.ciudad, nuevoValor, sizeof(persona.ciudad) - 1);
+                    break;
+                case 6:
+                    strncpy(persona.pais, nuevoValor, sizeof(persona.pais) - 1);
+                    break;
+                case 7:
+                    strncpy(persona.provincia, nuevoValor, sizeof(persona.provincia) - 1);
+                    break;
+                case 8:
+                    strncpy(persona.codigoPostal, nuevoValor, sizeof(persona.codigoPostal) - 1);
+                    break;
+                case 9:
+                    strncpy(persona.telefono1, nuevoValor, sizeof(persona.telefono1) - 1);
+                    break;
+                case 10:
+                    strncpy(persona.telefono2, nuevoValor, sizeof(persona.telefono2) - 1);
+                    break;
+                case 11:
+                    strncpy(persona.email, nuevoValor, sizeof(persona.email) - 1);
+                    break;
+                case 12:
+                    strncpy(persona.web, nuevoValor, sizeof(persona.web) - 1);
+                    break;
                 default:
                     printf("Campo no válido.\n");
                     return;
@@ -123,15 +148,13 @@ void mostrarRegistroPersonaPorEmail(FILE *archivo, const char *email) {
 
     fseek(archivo, 0, SEEK_SET);  // Ir al principio del archivo
 
-    printf("Registro de la persona que tiene email: %s\n", persona.email);
-
     while (fread(&persona, sizeof(struct Persona), 1, archivo) == 1) {
         if (strcmp(persona.email, email) == 0) {
             // Persona encontrada, mostrar el registro
             printf("Nombre: %s %s\n", persona.nombre, persona.apellido);
             printf("Email: %s\n", persona.email);
             printf("Compañía: %s\n", persona.empresa);
-            printf("Dirección: %s, %s, %s, %s, %s\n", persona.direccion, persona.ciudad, persona.pais, persona.estado, persona.codigoPostal);
+            printf("Dirección: %s, %s, %s, %s, %s\n", persona.direccion, persona.ciudad, persona.pais, persona.provincia, persona.codigoPostal);
             printf("Teléfono 1: %s\n", persona.telefono1);
             printf("Teléfono 2: %s\n", persona.telefono2);
             printf("Sitio web: %s\n", persona.web);
@@ -148,8 +171,6 @@ void mostrarRegistrosPersonas(FILE *archivo) {
 
     fseek(archivo, 0, SEEK_SET);  // Ir al principio del archivo
     
-    printf("Registro de TODAS las Personas\n\n");
-
     int contador = 1;
     while (fread(&persona, sizeof(struct Persona), 1, archivo) == 1) {
         // Mostrar el registro de la persona
@@ -157,7 +178,7 @@ void mostrarRegistrosPersonas(FILE *archivo) {
         printf("Nombre: %s %s\n", persona.nombre, persona.apellido);
         printf("Email: %s\n", persona.email);
         printf("Compañía: %s\n", persona.empresa);
-        printf("Dirección: %s, %s, %s, %s, %s\n", persona.direccion, persona.ciudad, persona.pais, persona.estado, persona.codigoPostal);
+        printf("Dirección: %s, %s, %s, %s, %s\n", persona.direccion, persona.ciudad, persona.pais, persona.provincia, persona.codigoPostal);
         printf("Teléfono 1: %s\n", persona.telefono1);
         printf("Teléfono 2: %s\n", persona.telefono2);
         printf("Sitio web: %s\n", persona.web);
@@ -170,22 +191,145 @@ int main(){
     const char *nombreArchivo = "registro_persona.bin";
     FILE *archivo = abrirArchivo(nombreArchivo, "wb+");
 
-    struct Persona persona1 = {"John", "Doe", "Company1", "Address1", "City1", "County1", "State1", "12345", "111-111-1111", "222-222-2222", "john@example.com", "www.example.com"};
-    struct Persona persona2 = {"Jane", "Doe", "Company2", "Address2", "City2", "County2", "State2", "54321", "333-333-3333", "444-444-4444", "jane@example.com", "www.example.org"};
+    bool flag = true;
+    int opcion = 0;
+    char email[50];
 
-    agregarPersona(archivo, &persona1);
-    agregarPersona(archivo, &persona2);
 
-    modificarRegistroPersona(archivo, "john@example.com", 1, "Jonathan");
-    
-    buscarPorEmail(archivo, "john@example.com");
-    buscarPorEmail(archivo, "jane@example.org");
+    while (flag) {
+        printf("-------------- Menú ---------------\n\n");
+        printf("(1) Agregar Registro Persona\n");
+        printf("(2) Modificar Registro Persona (por Email)\n");
+        printf("(3) Buscar Persona (por Email)\n");
+        printf("(4) Mostrar Registro Persona (por Email)\n");
+        printf("(5) Mostrar Registros Personas\n");
+        printf("(0) Salir\n");
+        printf("Ingrese una opción: ");
+        scanf("%d", &opcion);
 
-    mostrarRegistroPersonaPorEmail(archivo, "john@example.com");
+        struct Persona persona;
+        switch (opcion) {
+            case 1:
+                printf("----------------------------");
+                printf("\n| Agregar Registro Persona |\n");
+                printf("----------------------------\n");
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
 
-    printf("\n");
+                printf("---> Nombre: ");
+                fgets(persona.nombre, sizeof(persona.nombre), stdin);
+                persona.nombre[strcspn(persona.nombre, "\n")] = '\0';
 
-    mostrarRegistrosPersonas(archivo);
+                printf("---> Apellido: ");
+                fgets(persona.apellido, sizeof(persona.apellido), stdin);
+                persona.apellido[strcspn(persona.apellido, "\n")] = '\0';
+
+                printf("---> Empresa: ");
+                fgets(persona.empresa, sizeof(persona.empresa), stdin);
+                persona.empresa[strcspn(persona.empresa, "\n")] = '\0';
+
+                printf("---> Direccion: ");
+                fgets(persona.direccion, sizeof(persona.direccion), stdin);
+                persona.direccion[strcspn(persona.direccion, "\n")] = '\0';
+
+                printf("---> Ciudad: ");
+                fgets(persona.ciudad, sizeof(persona.ciudad), stdin);
+                persona.ciudad[strcspn(persona.ciudad, "\n")] = '\0';
+
+                printf("---> Pais: ");
+                fgets(persona.pais, sizeof(persona.pais), stdin);
+                persona.pais[strcspn(persona.pais, "\n")] = '\0';
+
+                printf("---> Provincia: ");
+                fgets(persona.provincia, sizeof(persona.provincia), stdin);
+                persona.provincia[strcspn(persona.provincia, "\n")] = '\0';
+
+                printf("---> Codigo Postal: ");
+                fgets(persona.codigoPostal, sizeof(persona.codigoPostal), stdin);
+                persona.codigoPostal[strcspn(persona.codigoPostal, "\n")] = '\0';
+
+                printf("---> Primer Telefono: ");
+                fgets(persona.telefono1, sizeof(persona.telefono1), stdin);
+                persona.telefono1[strcspn(persona.telefono1, "\n")] = '\0';
+
+                printf("---> Segundo Telefono: ");
+                fgets(persona.telefono2, sizeof(persona.telefono2), stdin);
+                persona.telefono2[strcspn(persona.telefono2, "\n")] = '\0';
+
+                printf("---> Email: ");
+                fgets(persona.email, sizeof(persona.email), stdin);
+                persona.email[strcspn(persona.email, "\n")] = '\0';
+
+                printf("---> Web: ");
+                fgets(persona.web, sizeof(persona.web), stdin);
+                persona.web[strcspn(persona.web, "\n")] = '\0';
+
+                agregarPersona(archivo, &persona);
+                break;
+
+            case 2:
+                printf("------------------------------------------");
+                printf("\n| Modificar Registro Persona (por Email) |\n");
+                printf("------------------------------------------\n");
+                printf("\n---> Ingrese el email de la persona a modificar: ");
+                scanf("%s", email);
+
+                while ((c = getchar()) != '\n' && c != EOF);
+                int campo = 0;
+                
+                printf("Campo 1: Nombre\nCampo 2: Apellido\nCampo 3: Empresa\nCampo 4: Direccion\nCampo 5: Ciudad");
+                printf("\nCampo 6: Pais\nCampo 7: Provincia\nCampo 8: Codigo Postal\nCampo 9: Primer Telefono\nCampo 10: Segundo Telefono");
+                printf("\nCampo 11: Email\nCampo 12: Web");
+
+                printf("\n---> Ingrese el campo del registro a modificar: ");
+                scanf("%d", &campo);
+
+                char nuevo[100];
+                printf("\n---> Ingrese el nuevo valor: ");
+                getchar();
+                fgets(nuevo, sizeof(nuevo), stdin);
+                nuevo[strcspn(nuevo, "\n")] = '\0';
+                modificarRegistroPersona(archivo, email, campo, nuevo);
+                break;
+
+            case 3:
+                printf("------------------------------");
+                printf("\n| Buscar Persona (por Email) |\n");
+                printf("------------------------------\n");
+                printf("\n---> Ingrese el email de la persona a buscar: ");
+                scanf("%s", email);
+                while ((c = getchar()) != '\n' && c != EOF);
+                buscarPorEmail(archivo, email);
+                break;
+
+            case 4:
+                printf("----------------------------------------");
+                printf("\n| Mostrar Registro Persona (por Email) |\n");
+                printf("----------------------------------------\n");
+                printf("\n---> Ingrese el email de la persona a mostrar: ");
+                scanf("%s", email);
+                while ((c = getchar()) != '\n' && c != EOF);
+                cuentaRegresiva(2);
+                mostrarRegistroPersonaPorEmail(archivo, email);
+                break;
+
+            case 5:
+                printf("------------------------------");
+                printf("\n| Mostrar Registros Personas |\n");
+                printf("------------------------------\n");
+                while ((c = getchar()) != '\n' && c != EOF);
+                cuentaRegresiva(2);
+                mostrarRegistrosPersonas(archivo);
+                break;
+
+            case 0:
+                printf("SALIENDO...");
+                flag = false;
+                break;
+            default:
+                printf("\nOpcion invalida.\n");
+        }
+    }
     cerrarArchivo(archivo);
 
     return 0;
